5bbcab1d15b71d38f2fc2b0854a354c7402b9a8c
Qra <74237145+Qradam@users.noreply.github.com>
2025-06-19 00:29:39 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/content',
-    '@nuxt/eslint', 
+    '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxtjs/tailwindcss'
   ],
-
   content: {
     documentDriven: false,
     studio: true
   },
-
   css: ['~/assets/css/main.css']
 })
